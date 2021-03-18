dfc01cec4a6c832f2dd910d760ae4e916c7af8e5
Tom-wsl2-desktop <tomcarahermusic@gmail.com>
2021-03-18 19:33:39 +0000

    changed function names, added support for styled components, simplified central useInterval function

---
+++ src/tunerStyles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const tunerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 500px;
+  width: 800px;
+  margin: auto;
+  border: 5px solid white;
+`
+
+export const tunerLine = styled.div`
+  .ref-hr {
+    border: 15px solid green;
+    width: 700px;
+  }
+`
